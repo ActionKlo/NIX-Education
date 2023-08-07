1e5a545b14450ea78ac78a1c7bb02d3cfcd5f77e
Andrei <actionclaw@gmail.com>
2023-08-07 19:28:52 +0200

    Get 5 posts from jsonplaceholder via goroutines

--- cmd/nix/main.go
+++ cmd/nix/main.go
@@ -5,25 +5,53 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
 func main() {
-	res, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	ch := make(chan string)
+	var wg sync.WaitGroup
 
-	if err != nil {
-		log.Fatalln(err)
-		return
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+
+		go func(i int) {
+			defer wg.Done()
+
+			res, err := http.Get("https://jsonplaceholder.typicode.com/posts/" + strconv.Itoa(i))
+
+			if err != nil {
+				log.Fatalln(err)
+				return
+			}
+
+			defer func(Body io.ReadCloser) {
+				err = Body.Close()
+				if err != nil {
+					log.Fatalln(err)
+					return
+				}
+			}(res.Body)
+
+			body, err := io.ReadAll(res.Body)
+			if err != nil {
+				log.Fatalln(err)
+				return
+			}
+
+			sb := string(body)
+
+			ch <- sb
+		}(i)
 	}
 
-	defer func(Body io.ReadCloser) {
-		err = Body.Close()
-		if err != nil {
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
 
-		}
-	}(res.Body)
-
-	body, err := io.ReadAll(res.Body)
-	sb := string(body)
-
-	fmt.Println(sb)
+	for b := range ch {
+		fmt.Println(b)
+	}
 }
